a36e9aa0f9d6687d336c54d080dbe7ea79ede3ce
Marcos Minond <minond.marcos@gmail.com>
2023-03-05 00:09:56 -0700

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+
+(define collection "reader")
+(define build-deps '("rackunit-lib"))
+(define deps '("base" "deta"))
+; (define deps '("deta" "request" "gregor" "css-expr" "crypto" "rfc6455"))
