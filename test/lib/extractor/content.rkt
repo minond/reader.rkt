c4391fbbfc3e45078ccf010915c50e2ea2afee15
Marcos Minond <minond.marcos@gmail.com>
2023-03-12 17:05:30 -0600

    More extractor tests

--- test/lib/extractor/content.rkt
+++ test/lib/extractor/content.rkt
@@ -22,9 +22,19 @@
   (test-suite
    "Content extraction"
 
-   (test-case
-    "Basic HTML page"
-
-    (check-match
-     (car minond-xyz-same-adt-content)
-     (heading empty 1 (list (text "Same (ADT) type, different meaning")))))))
+   (check-match
+    minond-xyz-same-adt-content
+    (list (heading empty 1 (list (text "Same (ADT) type, different meaning")))
+          _ ...
+          (paragraph empty (list (text "There are instances where the semantics of distinct types overlap. Through ADTs and OOP, it is possible to represent this using different sets of types, while still being able to work with the set unions and intersections in a way that is type safe.")))
+          _ ...
+          (paragraph empty (list (text "Since the only constructors for ")
+                                 (code empty (list (text "Expr")))
+                                 (text " are ")
+                                 (code empty (list (text "Number")))
+                                 (text " and ")
+                                 (code empty (list (text "Arithmetic")))
+                                 (text ", this is an exhaustive match of all possible inputs. Let")
+                                 (entity 'rsquo)
+                                 (text "s try it out to make sure things work:")))
+          _ ...))))
