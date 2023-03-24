91c1db68176e40bae829726430504057203b2bc9
Marcos Minond <minond.marcos@gmail.com>
2023-03-24 02:33:56 -0600

    Mcfly dep

--- info.rkt
+++ info.rkt
@@ -2,4 +2,4 @@
 
 (define name "reader")
 (define build-deps '("rackunit-lib"))
-(define deps '("deta" "request" "gregor" "css-expr" "crypto" "rfc6455"))
+(define deps '("deta" "request" "gregor" "css-expr" "crypto" "rfc6455" "mcfly"))
