856672adff4dc4a6597169e6fda09d566c2d4314
Marcos Minond <minond.marcos@gmail.com>
2023-03-24 02:37:44 -0600

    Overeasy dep

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 
 (define name "reader")
-(define build-deps '("rackunit-lib"))
+(define build-deps '("rackunit-lib" "overeasy"))
 (define deps '("deta" "request" "gregor" "css-expr" "crypto" "rfc6455" "mcfly"))
