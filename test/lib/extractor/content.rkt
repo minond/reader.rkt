2d52ceb83a61be681c84eedbfbbe9413c3586706
Marcos Minond <minond.marcos@gmail.com>
2023-03-28 14:41:28 -0600

    Updating test content structure

--- test/lib/extractor/content.rkt
+++ test/lib/extractor/content.rkt
@@ -18,23 +18,27 @@
 (define minond-xyz-same-adt-content
   (extract-test-data data/minond-xyz-same-adt))
 
+
 (define tests
   (test-suite
    "Content extraction"
 
    (check-match
     minond-xyz-same-adt-content
-    (list (heading empty (list (text "Same (ADT) type, different meaning")) 1)
-          _ ...
-          (paragraph empty (list (text "There are instances where the semantics of distinct types overlap. Through ADTs and OOP, it is possible to represent this using different sets of types, while still being able to work with the set unions and intersections in a way that is type safe.")))
-          _ ...
-          (paragraph empty (list (text "Since the only constructors for ")
-                                 (code empty (list (text "Expr")))
-                                 (text " are ")
-                                 (code empty (list (text "Number")))
-                                 (text " and ")
-                                 (code empty (list (text "Arithmetic")))
-                                 (text ", this is an exhaustive match of all possible inputs. Let")
-                                 (entity 'rsquo)
-                                 (text "s try it out to make sure things work:")))
-          _ ...))))
+    (division
+     empty
+     (list
+      (heading empty (list (text "Same (ADT) type, different meaning")) 1)
+      _ ...
+      (paragraph empty (list (text "There are instances where the semantics of distinct types overlap. Through ADTs and OOP, it is possible to represent this using different sets of types, while still being able to work with the set unions and intersections in a way that is type safe.")))
+      _ ...
+      (paragraph empty (list (text "Since the only constructors for ")
+                             (code empty (list (text "Expr")))
+                             (text " are ")
+                             (code empty (list (text "Number")))
+                             (text " and ")
+                             (code empty (list (text "Arithmetic")))
+                             (text ", this is an exhaustive match of all possible inputs. Let")
+                             (entity 'rsquo)
+                             (text "s try it out to make sure things work:")))
+      _ ...)))))
