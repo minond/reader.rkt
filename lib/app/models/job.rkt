c4122f8b1fb0f3a0a2f3c2c73341deea9c68e026
Marcos Minond <minond.marcos@gmail.com>
2023-04-10 23:28:06 -0600

    Job infrastructure

--- lib/app/models/job.rkt
+++ lib/app/models/job.rkt
@@ -14,48 +14,83 @@
 
 (provide (except-out (schema-out job)
                      make-job
+                     job-name
                      job-data)
-         (rename-out [make-job-override make-job]
-                     [job-data-override job-data])
-         acquire-next-job)
+         (rename-out [make-job-override make-job])
+         job-command
+         job-label
+         acquire-next-job!
+         job-completed!
+         job-errored!)
+
+(define-values (ready running completed errored)
+  (values "ready" "running" "completed" "errored"))
 
 (define-schema job
   ([id id/f #:primary-key #:auto-increment]
+   [status string/f #:contract non-empty-string?]
    [name string/f #:contract non-empty-string?]
    [data binary/f]
+   [logs string/f #:nullable]
    [started-at datetime/f #:nullable]
    [completed-at datetime/f #:nullable]
    [(created-at (now/utc)) datetime/f]))
 
-(define/contract (make-job-override #:data data)
-  (-> #:data serializable? job?)
-  (make-job #:name (symbol->string (object-name data))
-            #:data (s-exp->fasl (serialize data))))
+(define/contract (make-job-override #:command command)
+  (-> #:command serializable? job?)
+  (make-job #:status ready
+            #:name (symbol->string (object-name command))
+            #:data (s-exp->fasl (serialize command))))
 
-(define/contract (job-data-override job)
+(define/contract (job-label job)
+  (-> job? symbol?)
+  (string->symbol (job-name job)))
+
+(define/contract (job-command job)
   (-> job? serializable?)
   (deserialize (fasl->s-exp (job-data job))))
 
-(define (acquire-next-job [conn (current-database-connection)])
+(define (acquire-next-job! [conn (current-database-connection)])
   (let/cc return
-    (define job (lookup conn (find-available-job)))
+    (define job (lookup conn (find-ready-job)))
     (unless (job? job)
       (return #f))
 
-    (define results (query conn (update-job-as-unavailable job)))
+    (define results (query conn (update-job-as-running job)))
     (define affected-rows (cdr (assq 'affected-rows (simple-result-info results))))
     (unless (eq? 1 affected-rows)
       (return #f))
 
     job))
 
-(define (find-available-job)
+(define (job-completed! job logs [conn (current-database-connection)])
+  (query conn (update-job-as-completed job logs)))
+
+(define (job-errored! job logs [conn (current-database-connection)])
+  (query conn (update-job-as-errored job logs)))
+
+(define (find-ready-job)
   (~> (from job #:as j)
-      (where (is j.started-at null))
+      (where (is j.status ,ready))
       (limit 1)))
 
-(define (update-job-as-unavailable job)
+(define (update-job-as-running job)
   (~> (from job #:as j)
-      (update [started-at ,(~t (now/utc) "yyyy-MM-dd'T'HH:mm:ss")])
-      (where (and (is j.started-at null)
+      (update [status ,running]
+              [started-at ,(~t (now/utc) "yyyy-MM-dd'T'HH:mm:ss")])
+      (where (and (is j.status ,ready)
                   (= j.id ,(job-id job))))))
+
+(define (update-job-as-completed job logs)
+  (~> (from job #:as j)
+      (update [status ,completed]
+              [logs ,logs]
+              [completed-at ,(~t (now/utc) "yyyy-MM-dd'T'HH:mm:ss")])
+      (where (= j.id ,(job-id job)))))
+
+(define (update-job-as-errored job logs)
+  (~> (from job #:as j)
+      (update [status ,errored]
+              [logs ,logs]
+              [completed-at ,(~t (now/utc) "yyyy-MM-dd'T'HH:mm:ss")])
+      (where (= j.id ,(job-id job)))))
---
+++ lib/job.rkt
@@ -0,0 +1,62 @@
+#lang racket/base
+
+(require reader/lib/random
+         reader/lib/parameters
+         reader/lib/app/models/job)
+
+(provide make-job-manager
+         register-job-handler!
+         (all-from-out reader/lib/app/models/job))
+
+(define handlers (make-hash))
+(define (register-job-handler! name f)
+  (define label
+    (cond [(symbol? name) name]
+          [(string? name) (string->symbol name)]
+          [else (object-name name)]))
+  (hash-set! handlers label f))
+
+(define (make-job-manager #:cust [cust (current-custodian)])
+  (define id (random-string))
+  (define thd
+    (parameterize ([current-custodian cust])
+      (thread
+       (lambda ()
+         (log-info "starting worker:~a" id)
+         (let loop ()
+           (sync
+            (handle-evt (thread-receive-evt)
+                        (lambda (_)
+                          (log-info "stopping worker:~a" id)))
+            (handle-evt (alarm-evt (+ (current-inexact-milliseconds) 1000))
+                        (lambda (args)
+                          (define job (acquire-next-job! (current-database-connection)))
+                          (when job (safe-run-job job))
+                          (loop)))))))))
+
+  (lambda ()
+    (thread-send thd 'stop)))
+
+(define (safe-run-job job)
+  (define errored? #f)
+  (define buf (open-output-string))
+
+  (parameterize ([current-output-port buf]
+                 [current-error-port buf])
+    (with-handlers ([exn:fail?
+                     (lambda (e)
+                       (set! errored? #t)
+                       (displayln (exn-message e)))])
+      (run-job job)))
+
+  (if errored?
+      (job-errored! job (get-output-string buf))
+      (job-completed! job (get-output-string buf))))
+
+(define (run-job job)
+  (define name (job-label job))
+  (unless (hash-has-key? handlers name)
+    (error "job handler not found"))
+
+  (define handler (hash-ref handlers name))
+  (handler (job-command job)))
