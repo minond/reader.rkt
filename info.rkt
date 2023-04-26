0927980d41f7927e4257ca182c9d4c5368682d2b
Marcos Minond <minond.marcos@gmail.com>
2023-04-25 18:26:17 -0600

    Declaring new deps

--- info.rkt
+++ info.rkt
@@ -2,4 +2,8 @@
 
 (define name "reader")
 (define build-deps '("rackunit-lib" "overeasy"))
-(define deps '("deta" "request" "gregor" "css-expr" "crypto" "rfc6455" "mcfly" "markdown" "uuid"))
+(define deps '("deta" "request" "gregor" "css-expr" "crypto" "rfc6455" "markdown" "uuid"
+               ; html-parsing
+               "mcfly"
+               ; openapi
+               "http-easy" "monotonic" "yaml"))
