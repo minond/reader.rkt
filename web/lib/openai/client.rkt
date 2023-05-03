02c799ef0b4da5461d187c5808c051e1e88000b3
Marcos Minond <minond.marcos@gmail.com>
2023-05-03 15:27:29 -0600

    Generate article tags command

--- web/lib/openai/client.rkt
+++ web/lib/openai/client.rkt
@@ -1,9 +1,24 @@
 #lang racket/base
 
-(require reader/thirdparty/openapi/client)
+(require threading
+         reader/thirdparty/openapi/client)
 
-(provide create-chat-completion)
+(provide create-chat-completion
+         first-message-content
+         trim-prompt-text)
 
 (openapi openai "lib/openai/openapi.yaml"
          #:only (create-chat-completion)
          #:bearer (getenv "OPENAI_API_KEY"))
+
+(define (first-message-content response)
+  (~> response
+      (hash-ref 'choices)
+      (car)
+      (hash-ref 'message)
+      (hash-ref 'content)))
+
+(define (trim-prompt-text str)
+  (if (< (string-length str) 4000)
+      str
+      (substring str 0 4000)))
